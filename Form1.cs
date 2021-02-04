33529c0c4ea53780ce5917ab80383c32023395f4
Opilane TTHK <opilane@tthk.ee>
2021-02-04 10:43:53 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kino
+{
+    public partial class Form1 : Form
+    {
+        int i, j;
+        Label lbl;
+        Button btnS, btnM, btnL;
+        public Form1()
+        {
+            Height = 150;
+            Width = 360;
+            Text = "Saali valimine";
+
+            lbl = new Label();
+            lbl.Text = "Vali saal:";
+            lbl.Size = new Size(300, 20);
+            lbl.Location = new Point(20, 10);
+            Controls.Add(lbl);
+
+            btnS = new Button();
+            btnS.Text = "Väike";
+            btnS.Size = new Size(100, 30);
+            btnS.Location = new Point(20, 40);
+            btnS.Click += BtnS_Click;
+            Controls.Add(btnS);
+
+            btnM = new Button();
+            btnM.Text = "Keskmine";
+            btnM.Size = new Size(100, 30);
+            btnM.Location = new Point(120, 40);
+            btnM.Click += BtnM_Click;
+            Controls.Add(btnM);
+
+            btnL = new Button();
+            btnL.Text = "Suur";
+            btnL.Size = new Size(100, 30);
+            btnL.Location = new Point(220, 40);
+            btnL.Click += BtnL_Click;
+            Controls.Add(btnL);
+        }
+
+        private void BtnL_Click(object sender, EventArgs e)
+        {
+            i = 10; j = 10;
+            Saalid hall = new Saalid(i, j);
+            hall.Show();
+            Hide();
+        }
+
+        private void BtnM_Click(object sender, EventArgs e)
+        {
+            i = 5; j = 5;
+            Saalid hall = new Saalid(i, j);
+            hall.Show();
+            Hide();
+        }
+
+        private void BtnS_Click(object sender, EventArgs e)
+        {
+            i = 3; j = 3;
+            Saalid hall = new Saalid(i, j);
+            hall.Show();
+            Hide();
+        }
+    }
+}
---
+++ Saalid.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kino
+{
+    public partial class Saalid : Form
+    {
+        int i, j;
+        Label[,] _arr;
+        public Saalid(int i_, int j_)
+        {
+            _arr = new Label[i_, j_];
+            Size = new Size(i_ * 80, j_ * 75);
+            Text = "Saal";
+            for (i = 0; i < i_; i++)
+            {
+                for (j = 0; j < j_; j++)
+                {
+                    _arr[i, j] = new Label();
+                    _arr[i, j].BackColor = Color.DarkGreen;
+                    _arr[i, j].Text = "Koht " + (j + 1);
+                    _arr[i, j].TextAlign = ContentAlignment.MiddleCenter;
+                    _arr[i, j].Size = new Size(55, 55);
+                    _arr[i, j].BorderStyle = BorderStyle.Fixed3D;
+                    _arr[i, j].Location = new Point(j * 55 + 55, i * 55);
+                    Controls.Add(_arr[i, j]);
+                    _arr[i, j].Tag = new int[] { i, j };
+                    _arr[i, j].Click += new System.EventHandler(Saalid_Click);
+                }
+            }
+        }
+
+        private void Saalid_Click(object sender, EventArgs e)
+        {
+            var label = (Label)sender;
+            var tag = (int[])label.Tag;
+            if (_arr[tag[0], tag[1]].Text != "Valitud")
+            {
+                _arr[tag[0], tag[1]].Text = "Valitud";
+                _arr[tag[0], tag[1]].BackColor = Color.DarkOrange;
+            }
+            else
+            {
+                MessageBox.Show("Rida: " + (tag[0] + 1) + ", Koht: " + (tag[1] + 1) + " - juba võetud!");
+            }
+        }
+    }
+}
